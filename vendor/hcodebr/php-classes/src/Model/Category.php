cb75a9f4bfde3ea3ddb50476c148417ae2730973
Camila <camila.perellon@gmail.com>
2019-02-12 10:29:47 -0200

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -100,6 +100,33 @@ class Category extends Model{
         }
     }
 
+    public function getProductPage($page=1, $itemsPerPage = 8){
+
+        $start = ($page-1) * $itemsPerPage; 
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_products a
+            INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+            INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+            WHERE c.idcategory = :idcategory
+            LIMIT $start, $itemsPerPage;", [
+                ':idcategory'=>$this->getidcategory()
+            ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>Product::checkList($results),
+            'total'=>(int)$resultTotal[0]["nrtotal"],
+            'pages'=>ceil($resultTotal[0]["nrtotal"]/$itemsPerPage)  //ceil- função que coverte arredondando para cima (inteiro)
+        ];
+
+        
+    }
+
     public function addProduct(Product $product){
 
         $sql = new Sql();
