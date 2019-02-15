63c13a2d1d42707b18fb6b7cdf7a07f8c3c83849
Camila <camila.perellon@gmail.com>
2019-02-15 15:24:13 -0200

    Alterar a Senha - Site

--- site.php
+++ site.php
@@ -604,4 +604,74 @@ $app->get("/profile/orders/:idorder", function($idorder){
 	]);
 });
 
+$app->get("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password", [
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+});
+
+$app->post("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	if(!isset($_POST['current_pass']) && $_POST['current_pass'] === ''){
+
+		User::setError("Digite a Senha Atual");
+
+		header("Location: /curso/Ecommerce/index.php/profile/change-password");
+		exit;
+	}
+
+		if(!isset($_POST['new_pass']) && $_POST['new_pass'] === ''){
+
+		User::setError("Digite a Nova Senha");
+
+		header("Location: /curso/Ecommerce/index.php/profile/change-password");
+		exit;
+	}
+
+		if(!isset($_POST['new_pass_confirm']) && $_POST['new_pass_confirm'] === ''){
+
+		User::setError("Confirme a Nova Senha");
+
+		header("Location: /curso/Ecommerce/index.php/profile/change-password");
+		exit;
+	}
+
+	if($_POST['current_pass'] === $_POST['new_pass']){
+
+		User::setError("A sua nova senha deve ser diferente da atual");
+
+		header("Location: /curso/Ecommerce/index.php/profile/change-password");
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if(!password_verify($_POST['current_pass'], $user->getdespassword())){
+
+		User::setError("Senha Inválida");
+
+		header("Location: /curso/Ecommerce/index.php/profile/change-password");
+		exit;
+	}
+
+	$user->setdespassword($_POST["new_pass"]);
+
+	$user->update();
+
+	User::setSuccess("Senha Alterada com Sucesso");
+
+	header("Location: /curso/Ecommerce/index.php/profile/change-password");
+	exit;
+});
+
+
+
 ?>
